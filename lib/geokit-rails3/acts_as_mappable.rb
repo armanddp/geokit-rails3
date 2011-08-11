ba71caa70bd9d1f870d305525ac24aab586d3182
jdejong <jdejong@sparknettech.com>
2011-08-11 16:12:21 -0400

    fixed the adapter

--- lib/geokit-rails3/acts_as_mappable.rb
+++ lib/geokit-rails3/acts_as_mappable.rb
@@ -66,8 +66,6 @@ module Geokit
       def adapter
         @adapter ||= begin
 		  if connection.adapter_name.downcase == 'seamless_database_pool'
-			  Rails.logger.debug connection.master_connection.adapter_name
-			  puts connection.master_connection.adapter_name
 			  require File.join(File.dirname(__FILE__), 'adapters', connection.master_connection.adapter_name.downcase)
 			  klass = Adapters.const_get(connection.master_connection.adapter_name.camelcase)
 			  klass.load(self) unless klass.loaded
